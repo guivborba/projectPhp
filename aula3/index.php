e3655ad8364abb129a295945f7ea34bbd381fb74
Guilherme Borba <guivborba@hotmail.com>
2023-08-29 20:28:06 -0300

    Add files via upload

--- aula3/index.php
+++ aula3/index.php
@@ -1,11 +1,18 @@
 <?php
 
-$classe = $_GET['controller'];
-$metodo = $_GET['acao'];
+const FOLDER = 'aula3';
 
-$classe .= 'Controller';
+if(isset($_GET['controller']) && isset($_GET['acao'])){
+    $controller = $_GET['controller'];
+    $metodo = $_GET['acao'];
+    $controller .= 'Controller';
 
-require_once 'controller/'.$classe.'.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/' . FOLDER . '/controller/'.$controller.'.php';
 
-$estudanteController = new $classe();
-$estudanteController->$metodo();
+    $objeto = new $controller();
+    $objeto->$metodo();
+} else{
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/' . FOLDER . '/view/home.php';
+}
+
+
--- aula3/model/ProfessorModel.php
+++ aula3/model/ProfessorModel.php
@@ -1,6 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/' . FOLDER . '/database/Database.php';
-class EstudanteModel {
+class ProfessorModel {
     public string $nome;
     public int $idade;
     public $database;
@@ -11,13 +11,13 @@ class EstudanteModel {
 
     public function listar(){
 
-        $dadosArray = $this -> database -> executeSelect("SELECT * FROM estudantes");
+        $dadosArray = $this -> database -> executeSelect("SELECT * FROM professores");
         return $dadosArray;
     }
 
     public function salvarModel(string $nome, int $idade){
 
-        $sql = "INSERT INTO estudantes (nome, idade) values ('$nome', '$idade')";
+        $sql = "INSERT INTO professores (nome, idade) values ('$nome', '$idade')";
         $this->database->insert($sql);
     }
 
